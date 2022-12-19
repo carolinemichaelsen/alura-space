9825d4be7e84721fe65ccdbe119bed874435dc2f
Caroline Michaelsen <117784264+carolinemichaelsen@users.noreply.github.com>
2022-12-19 16:48:22 -0300

    galeria e fotos populares

--- src/Pages/HomePage/index.jsx
+++ src/Pages/HomePage/index.jsx
@@ -1,29 +1,30 @@
-import banner from './banner.png'
-import Header from "../../components/Header";
-import Menu from "../../components/Menu";
-import styles from './HomePage.module.scss'
-import Footer from '../../components/Footer';
-import Galeria from '../../components/Galeria';
+import banner from "./banner.png";
+import styles from "./HomePage.module.scss";
 
-export default function HomePage(){
-    return (
-        <>
-            <Header />
-            <main>
-                <section className={styles.principal}>
-                <Menu />
-                <div className={styles.principal__imagem}>
-                    <h1>A galeria mais completa do espaço</h1>
-                    <img src={banner} alt="Imagem da Terra vista do espaço" />
-                </div>
-                </section>
-            <div className={styles.galeria}>
-                <Galeria />
-            </div>
-            < Footer />
-            
-            </main>
-            
-        </>
-        )
+import Header from "components/Header";
+import Menu from "components/Menu";
+import Footer from "components/Footer";
+import Galeria from "components/Galeria";
+import Populares from "components/Populares";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="Imagem da Terra vista do espaço" />
+          </div>
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
